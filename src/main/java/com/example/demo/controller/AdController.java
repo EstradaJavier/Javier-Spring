47c8884b67fcd0b8efbb462d6f2035b0415554bf
javier estrada <estrada.javier.2022@gmail.com>
2021-05-07 11:41:10 -0500

    feat create html

--- src/main/java/com/example/demo/controller/AdController.java
+++ src/main/java/com/example/demo/controller/AdController.java
@@ -1,4 +1,24 @@
 package com.example.demo.controller;
 
+import com.example.demo.repositories.AdRepository;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
 public class AdController {
+    @GetMapping("/ads/create")
+    public String showCreateForm() {
+        return "ads/create";
+    }
+
+    @PostMapping("/ads/create")
+    public String create(
+            @RequestParam(name = "title") String title,
+            @RequestParam(name = "description") String description
+    ) {
+        Ad ad = new Ad();
+        ad.setTitle(title);
+        ad.setDescription(description);
+        // save the ad...
+    }
 }
